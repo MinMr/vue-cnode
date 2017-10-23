8906c21d08ac5d242c984effb4bd1b0a1c5518c9
XiangMin <15611854455@163.com>
2017-10-23 17:37:33 +0800

    添加element-ui

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
  * @File: 入口文件
  * @Date: 2017-10-15 20:49:19 
  * @Last Modified by: xiangmin
- * @Last Modified time: 2017-10-16 17:08:47
+ * @Last Modified time: 2017-10-23 17:35:57
  */
 
 import Vue from 'vue';
 import App from './App';
 import 'style/reset.css' 
 import router from './router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
